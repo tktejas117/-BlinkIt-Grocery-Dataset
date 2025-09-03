90a0209bb9081a98f80fae7863be5035e1a03d6d
tejas_kumar <109674177+tktejas117@users.noreply.github.com>
2025-09-03 14:21:39 +0530

    Add files via upload

---
+++ Buiness problems KPI's.sql
@@ -0,0 +1,25 @@
+-- Buiness problems KPI's
+
+Blinkit Business Problems & Solutions
+KPI’s
+1. Total Sales – The overall revenue generated from all items sold
+2. Average Sales – The average revenue per sale.
+3. Number of Items – The total count of different items sold.
+4. Average Rating – The average customer rating for items sold.
+
+Granular Analysis
+1. Total Sales by Fat Content – Analyze the impact of fat content (Low Fat, Regular) on total sales.
+
+2. Total Sales by Item Type – Identify which product categories contribute most to sales.
+
+3. Fat Content by Outlet for Total Sales – Compare total sales across outlets segmented by fat content.
+
+4. Total Sales by Outlet Establishment – Evaluate how the age or type of outlet influences sales.
+
+Charts / Advanced KPIs
+
+1. Percentage of Sales by Outlet Size – Correlation between outlet size (Small, Medium, Large) and sales.
+
+2. Sales by Outlet Location – Geographic distribution of sales across different locations.
+
+3. All Metrics by Outlet Type – Compare all KPIs (Total Sales, Avg Sales, Number of Items, Avg Rating, Visibility) across outlet types.
